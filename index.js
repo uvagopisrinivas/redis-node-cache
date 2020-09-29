1de8b04b473bdf43ccded41c171c3acfe823bc5f
PC <pc@yuvas-mbp-2.home>
2020-09-28 22:14:09 -0400

    Prg: Using node-fetch & expresss get gethub profile data

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.get('/repos/:username', getRepos)
+async function getRepos (req, res, next) {
+    try {
+        console.log('Fetching Data...');
+        const { username } = req.params;
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const data = await response.json();
+        res.send(data);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+    }
+}
+
 app.listen(5000, () => {
     console.log(`App listening to the port ${PORT}`);
 });
-
-
 
